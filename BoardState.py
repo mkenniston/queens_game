f62d2bff05c2a0497673838ee3c534a3c1114b31
Michael S. Kenniston <git@michaelkenniston.com>
2025-07-13 23:45:11 -0400

    Added MIT copyright notice, and split BoardState ctor

--- BoardState.py
+++ BoardState.py
@@ -1,10 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright © 2025 Michael S. Kenniston.  All rights reserved.
+
+"""
+Copyright 2025 Michael S. Kenniston
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the “Software”),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
 
 # Define the current state of a "Queens" game
 # The "BoardState" object contains all the mutable information.
 
+
+from BoardGeom import BoardGeom
 
 FREE = ' '
 BLOCKED = 'x'
@@ -13,10 +36,33 @@ ALL_STATES = [FREE, BLOCKED, QUEEN]
 
 
 class BoardState():
-    def __init__(self, board_geom):
+    def __init__(self, arg):
+        if isinstance(arg, BoardGeom):
+            self._init_new(arg)
+        elif isinstance(arg, BoardState):
+            self._init_copy(arg)
+        else:
+            raise BaseException("illegal arg type %s" % type(arg))
+        pass
+        return
+
+    def _init_copy(self, new_copy):
+        new_copy._board_geom = self._board_geom
+        new_copy._groups = self._groups
+        size = self._board_geom.size()
+        self._cell_states = []
+        for row in range(size):
+            one_row = []
+            for col in range(size):
+                one_row.append(self._cell_states[row][col])
+            new_copy._cell_states.append(one_row)
+        new_copy.recalc_free_cells()
+        new_copy._num_queens = self._num_queens
+
+    def _init_new(self, board_geom):
         self._board_geom = board_geom
-        self._groups = board_geom.groups()
-        size = board_geom.size()
+        self._groups = self._board_geom.groups()
+        size = self._board_geom.size()
         self._cell_states = [[FREE] * size for i in range(size)]
         self.recalc_free_cells()
         self._num_queens = 0
